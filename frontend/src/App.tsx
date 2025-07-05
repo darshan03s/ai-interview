88004fde4b2995cdefc3251ff4a4b02369e6280b
Darshan S <darshans2727@gmail.com>
2025-07-05 23:14:51 +0530

    added protected route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { Home, Interview } from "@/pages";
 import { Toaster } from "./components/ui/sonner";
 import Layout from "./components/layout/Layout";
+import { useAuth } from "./features/auth/useAuth";
+import { useEffect } from "react";
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+  const { session, authLoading } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!authLoading && !session) {
+      navigate("/");
+    }
+  }, [session, navigate, authLoading]);
+
+  return children;
+};
 
 const App = () => {
   return (
@@ -9,7 +24,7 @@ const App = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route index path="/" element={<Home />} />
-          <Route path="/interview/:interviewId" element={<Interview />} />
+          <Route path="/interview/:interviewId" element={<ProtectedRoute><Interview /></ProtectedRoute>} />
         </Route>
       </Routes>
       <Toaster />
